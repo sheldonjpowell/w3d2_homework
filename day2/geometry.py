518077819172b03519d9524d78b029b423ed102d
Sheldon Powell <98640119+sheldonjpowell@users.noreply.github.com>
2022-03-09 09:06:39 -0600

    Add files via upload

---
+++ day2/geometry.py
@@ -0,0 +1,18 @@
+from cmath import pi
+import math
+
+
+# def circle(r):
+#     PI = 3.14
+#     return PI * (r*r)
+# print("Area of circle = %.6f" % circle(6))
+    
+
+    
+    
+    
+
+def right_angle():
+    side = 3,4
+print(math.hypot(3,4))
+        
